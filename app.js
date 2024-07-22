5352d99e54a08d66081ec14f6f6071782108de03
kevinMaga <kerimaga@espol.edu.ec>
2024-07-22 10:09:09 -0500

    Guia19

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-var createError = require('http-errors');
-var express = require('express');
+ /* 1. Carga de variables de entorno */
+ require('dotenv').config()
+
+ var createError = require('http-errors');
+ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
