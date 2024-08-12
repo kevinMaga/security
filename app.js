9d6e2f1d122b28b3cef795a07ee95ea0cb387e0f
kevinMaga <kerimaga@espol.edu.ec>
2024-08-12 17:25:14 -0500

    MysqL1

---
+++ app.js
@@ -0,0 +1,63 @@
+ /* 1. Carga de variables de entorno */
+ require('dotenv').config()
+
+ var createError = require('http-errors');
+ var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+ /* 1. Módulo express-session */
+ const session = require('express-session');
+   /* 1. Referencia a los middlewares */
+ var authenticateSession = require('./middleware/authentication_session');
+ var authorizationSession = require('./middleware/authorization_session');
+
+var indexRouter = require('./routes/index');
+var tokenRouter = require('./routes/token');
+var usersRouter = require('./routes/users');
+
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+ /* 2. Configuración del middleware */
+ app.use(session({
+  secret: process.env.TOKEN_SECRET,
+  name: 'session.security', 
+  resave: false,
+  saveUninitialized: false,
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+ /* 2. Agregue el middleware al router */
+ app.use('/users', authenticateSession, authorizationSession, usersRouter);
+ app.use('/', indexRouter);
+app.use('/token', tokenRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ middleware/authorization_session.js
@@ -0,0 +1,13 @@
+ /* Autorización */
+
+ var authorizationSession = (req, res, next) => {
+    if(process.env.ALL_GRANTED.includes(req.session.role)) {
+        return next()
+    } if(process.env.ALL_GRANTED2.includes(req.session.role)){
+        return res.redirect("/token")
+    } else{
+        return res.redirect("/")
+    }
+}
+
+module.exports = authorizationSession;
