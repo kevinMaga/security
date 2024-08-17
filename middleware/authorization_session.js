bb8d1976df05ce186a6bd8d241072bf237a24ba9
kevinMaga <kerimaga@espol.edu.ec>
2024-08-16 22:21:12 -0500

    modificacion 4

--- middleware/authorization_session.js
+++ middleware/authorization_session.js
@@ -1,12 +1,17 @@
- /* Autorización */
+/* Autorización */
 
- var authorizationSession = (req, res, next) => {
-    if(process.env.ALL_GRANTED.includes(req.session.role)) {
-        return next()
-    }else  if(process.env.ALL_USER.includes(req.session.role)) {
-        return res.redirect("/token")
-    } else{
-        return res.redirect("/")
+var authorizationSession = (req, res, next) => {
+    const userRole = req.session.role;
+
+    if(userRole === 'user') {
+        
+        return res.redirect("/token");
+    } else if(process.env.ALL_GRANTED.includes(userRole)) {
+       
+        return next();
+    } else {
+        
+        return res.redirect("/");
     }
 }
 
